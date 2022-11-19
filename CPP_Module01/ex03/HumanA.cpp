902afe68712e66e6afa6be704c76ef643db0b349
Anahit Apresyan <aapresya@c1r4s6.42yerevan.am>
2022-11-19 12:42:32 +0400

    Makefiles fixed

--- CPP_Module01/ex03/HumanA.cpp
+++ CPP_Module01/ex03/HumanA.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapresya <anahit.apresyan7@gmail.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 11:04:20 by aapresya          #+#    #+#             */
+/*   Updated: 2022/11/19 11:04:21 by aapresya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "HumanA.hpp"
 
 HumanA::HumanA(const std::string &name, Weapon &weapon): _weapon(weapon), _name(name){}
